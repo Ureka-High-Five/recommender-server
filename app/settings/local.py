33c3c740374c5222d23ed610a31a75e1c344079b
likerhythm <wjdalsdl0412@gmail.com>
2025-07-07 17:19:06 +0900

    chore: w2v 모델 정보 추가

--- app/settings/local.py
+++ app/settings/local.py
@@ -1,12 +1,18 @@
+# app/settings/local.py
+
 from pydantic_settings import BaseSettings
 
-# .env.local 설정 파일을 읽어와서 쉽게 접근할 수 있도록 해주는 인터페이스
 class LocalSettings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_USERNAME: str
     DB_PASSWORD: str
     DB_NAME: str
+    W2V_MODEL_PATH: str
+    RECOMMEND_CONTENTS_COUNT: int
 
     class Config:
         env_file = ".env.local"
+
+
+settings = LocalSettings()
