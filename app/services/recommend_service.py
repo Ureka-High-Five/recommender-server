6b61020e174763e4487900f614ede268ce3e033b
likerhythm <wjdalsdl0412@gmail.com>
2025-07-07 17:24:40 +0900

    feat: recommend_service 추가

---
+++ app/services/recommend_service.py
@@ -0,0 +1,31 @@
+import numpy as np
+from app.repositories import user_repository
+from app.repositories import content_repository
+from app.services import word2vec_util
+from app.settings import settings
+
+def contents(user_id: str) -> list[str]:
+  user_weights = user_repository.user_weight(user_id)
+  user_vector = word2vec_util.calc_user_vector(user_weights)
+  # 모든 contents_vector와 user_vector 비교
+  return find_similar_contents(user_vector)
+  
+def shorts(user_id : str) -> list[str]: # return shorts_id
+  return 0
+
+def find_similar_contents(user_vector):
+  content_vectors = content_repository.get_all_vector()
+
+  similarities = []
+  for content_id, content_data in content_vectors.items():
+    content_vector = content_data['embedding']
+    similarity = word2vec_util.calc_similarity(user_vector, content_vector)
+    similarities.append({
+                      "id": content_id,
+                      "title": content_data["title"],
+                      "thumbnail": content_data["thumbnail_url"],
+                      "score": similarity
+                    })
+
+  similarities.sort(key=lambda x: x["score"], reverse=True)
+  return similarities[:settings.RECOMMEND_CONTENTS_COUNT]
