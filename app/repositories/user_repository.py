66b57361e44610d309c2b99d7a7b90430e57cfe0
likerhythm <wjdalsdl0412@gmail.com>
2025-07-07 17:59:38 +0900

    refactor: UserNotFoundError 리팩토링

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,9 +1,11 @@
 from app.db import get_connection
-from app.exceptions.user_not_found_execption import UserNotFoundException
+from app.error.user_not_found_error import UserNotFoundError
+from app.error.weight_not_found_error import WeightNotFoundError
 
-def user_weight(user_id: str) -> dict[str, float]:
+def user_weight(user_id: int) -> dict[str, float]:
   with get_connection() as conn:
     with conn.cursor() as cursor:
+      user_id = find_user_by_id(user_id)
       sql = """
             SELECT m.name, p.weight
             FROM prefer_meta_info p JOIN meta_info m ON p.meta_info_id = m.id
@@ -16,3 +18,18 @@ def user_weight(user_id: str) -> dict[str, float]:
         raise UserNotFoundException(user_id)
       
       return {row['name']: row['weight'] for row in rows}
+
+def find_user_by_id(user_id: int) -> int:
+  with get_connection() as conn:
+    with conn.cursor() as cursor:
+      sql = """
+            SELECT id FROM users
+            WHERE id = %s
+            """
+      cursor.execute(sql, (user_id,))
+      row = cursor.fetchone()
+
+      if not row:
+        raise UserNotFoundError(user_id)
+      
+      return row['id']
