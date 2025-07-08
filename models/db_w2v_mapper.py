420253b2c2373b0473951fea707edca47ce7d371
likerhythm <wjdalsdl0412@gmail.com>
2025-07-09 00:48:50 +0900

    feat[#7]: db_w2v_mapper 구현
    db에 저장된 장르와 w2v 모델에 저장된 단어를 매핑합니다.

---
+++ models/db_w2v_mapper.py
@@ -0,0 +1,28 @@
+_genre_mapping = {
+  "공포": "Horror",
+  "액션": "Action",
+  "스릴러": "Thriller",
+  "미스터리": "Mystery",
+  "가족": "Family",
+  "코미디": "Comedy",
+  "애니메이션": "Animation",
+  "SF": "Science Fiction",
+  "다큐멘터리": "Documentary",
+  "판타지": "Fantasy",
+  "드라마": "Drama",
+  "모험": "Adventure",
+  "로맨스": "Romance",
+  "범죄": "Crime",
+  "역사": "History",
+  "음악": "Music",
+  "전쟁": "War",
+  "Action & Adventure": "Adventure",
+  "Sci-Fi & Fantasy": "Fantasy",
+  "Kids": "Children",
+  "War & Politics": "War",
+  "Talk": "Talkshow"
+}
+
+def translate_genre(korean_genre: str) -> str:
+  return _genre_mapping.get(korean_genre, "Unknown")  # 매핑 없을 경우 "Unknown" 반환
+
