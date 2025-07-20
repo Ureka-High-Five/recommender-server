20b072fe258f9eac4bce84abab884f0a6add8dea
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-21 03:44:07 +0900

    feat[#28] : 메세지를 받으면 json으로 파싱하여 선호 가중치를 업데이트 하는 로직 추가 (임시 주석 처리)

--- app/services/consumer.py
+++ app/services/consumer.py
@@ -3,6 +3,7 @@ import aio_pika
 
 QUEUE_NAME = "recommendation.weight.update"
 
+
 async def start_consumer():
     amqp_url = (
         f"amqp://{settings.RABBITMQ_DEFAULT_USER}:{settings.RABBITMQ_DEFAULT_PASS}"
@@ -17,4 +18,13 @@ async def start_consumer():
     async with queue.iterator() as queue_iter:
         async for message in queue_iter:
             async with message.process():
-                print(f"📥 Received: {message.body.decode()}")
+                body = message.body.decode()
+                print(f"📥 Received: {body}")
+                # try:
+                #     data = json.loads(body)
+                #     update_user_weight(
+                #         data,
+                #         repo=UserWeightRepository(mongo_client=settings.MONGO_CLIENT),
+                #     )
+                # except Exception as e:
+                #     print(f"Rabbit MQ로부터의 메세지를 처리하는데 실패하였습니다.: {e}")
