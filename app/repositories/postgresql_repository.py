ce171f573c5091fa759ea6f67fdad8b50e21921e
likerhythm <wjdalsdl0412@gmail.com>
2025-07-21 18:51:27 +0900

    feat[#39]: 쿼리 구현

---
+++ app/repositories/postgresql_repository.py
@@ -0,0 +1,13 @@
+import asyncpg
+from typing import List
+
+async def get_genres_by_content_id(pool: asyncpg.Pool, content_id: int) -> List[str]:
+    query = """
+        SELECT mi.name
+        FROM meta_info_contents mic
+        JOIN meta_info mi ON mic.meta_info_id = mi.id
+        WHERE mic.content_id = $1
+    """
+    async with pool.acquire() as conn:
+        rows = await conn.fetch(query, content_id)
+        return list({row["name"] for row in rows})
