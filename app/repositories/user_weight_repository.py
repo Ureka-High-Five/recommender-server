ee5137bb2fda8f967e4d9481e3f56c2c27c177f0
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-22 10:16:15 +0900

    refactor[#44] : MongoDB user_weight docs에 name 필드 추가

--- app/repositories/user_weight_repository.py
+++ app/repositories/user_weight_repository.py
@@ -2,20 +2,24 @@ from typing import List, Dict
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from pymongo import UpdateOne
 
+
 class UserWeightRepository:
     def __init__(self, mongo_client: AsyncIOMotorClient):
-        self.db = mongo_client["highfive"]
+        self.db = mongo_client["leadme"]
         self.collection: AsyncIOMotorCollection = self.db["user_weight"]
 
     def update_user_weights(
-        self, user_id: int, meta_info_ids: list[int], weight: float
+        self, user_id: int, meta_info: list[tuple[int, str]], weight: float
     ):
         operations = []
-        for meta_id in meta_info_ids:
+        for meta_id, name in meta_info:
             operations.append(
                 UpdateOne(
                     {"user_id": user_id, "meta_info_id": meta_id},
-                    {"$inc": {"weight": weight}},
+                    {
+                        "$inc": {"weight": weight},
+                        "$set": {"name": name},
+                    },
                     upsert=True,
                 )
             )
@@ -26,7 +30,7 @@ class UserWeightRepository:
         cursor = self.collection.find({"userId": user_id})
         results = await cursor.to_list(length=None)
         return results
-    
+
     async def reset_weight(self, user_id: int, genre: str, weight: float):
         filter = {"userId": user_id, "metaInfoName": genre}
         update = {"$set": {"weight": weight}}
