a57b86dc99b5c884cb1d92f3fa031b4bd149b9ff
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-22 10:12:21 +0900

    style[#44] : formatting 적용

--- app/main.py
+++ app/main.py
@@ -18,9 +18,11 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler()
 
+
 async def start_rabbitmq_consumer():
     print("🚀 RabbitMQ Consumer 시작")
     return asyncio.create_task(start_consumer())
+
 
 async def load_w2v(app: FastAPI):
     Word2VecModel.load_model(settings.W2V_MODEL_PATH)
@@ -37,7 +39,7 @@ async def load_w2v(app: FastAPI):
         port=settings.DB_PORT,
         database=settings.DB_NAME,
         user=settings.DB_USERNAME,
-        password=settings.DB_PASSWORD
+        password=settings.DB_PASSWORD,
     )
     app.state.pg_pool = pg_pool
     print("✅ PostgreSQL 연결 완료")
@@ -48,13 +50,14 @@ async def load_w2v(app: FastAPI):
             resize_weight(
                 action_log_repo,
                 user_weight_repo,
-                partial(get_genres_by_content_id, pg_pool)
+                partial(get_genres_by_content_id, pg_pool),
             )
         )
 
     scheduler.add_job(schedule_resize_weight, "cron", hour=3, minute=0)
     scheduler.start()
     print("✅ APScheduler 설정 완료")
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -73,6 +76,7 @@ async def lifespan(app: FastAPI):
     await close_redis()
     print("🧹 자원 정리 완료")
 
+
 app = FastAPI(
     title="recommender server",
     lifespan=lifespan,
@@ -83,10 +87,13 @@ app.include_router(content.router)
 app.include_router(user.router)
 app.include_router(embedding.router)
 
+
 @app.get("/")
 def read_root():
     return {"env": settings.DB_NAME}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
