31f46dc9cdf4297cb512c67f6f9467a631f6159b
likerhythm <wjdalsdl0412@gmail.com>
2025-07-23 13:33:58 +0900

    feat[#60]: dto 수정 및 레디스에 저장

--- app/router/user.py
+++ app/router/user.py
@@ -4,9 +4,14 @@ from pymongo import MongoClient
 from app.dto.user_dto import UserActionRequestDto
 from app.repositories.user_weight_repository import UserWeightRepository
 from app.services import user_service
+from app.services.redis import save_user_vector
 from app.settings import settings
 
 router = APIRouter()
+
+class OnboardingRequestDto(BaseModel):
+    user_id: int
+    genre_map: dict[str, int]
 
 class FastApiOnboardingResponseDto(BaseModel):
     userVector: str
@@ -18,8 +23,9 @@ def get_prefer_info_repository():
     return UserWeightRepository(mongo_client)
 
 @router.post("/user/preferences", response_model=FastApiOnboardingResponseDto)
-def onboarding(genre_map: dict[str, int]):
-    user_vector_str = user_service.init_user_vector(genre_map)
+def onboarding(req: OnboardingRequestDto):
+    user_vector_str = user_service.init_user_vector(req.genre_map)
+    save_user_vector(req.user_id, user_vector_str)
     return FastApiOnboardingResponseDto(userVector=user_vector_str)
 
 @router.patch("/user/action")
