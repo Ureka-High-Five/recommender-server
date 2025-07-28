6454d0964961c9324fcff154ab465707ce0b5ff4
sangyunpark99 <psangyun5108@gmail.com>
2025-07-28 13:06:51 +0900

    feat[#73]: update_user_weight 롤백 로직 적용

--- app/services/consumer.py
+++ app/services/consumer.py
@@ -1,8 +1,9 @@
 import json
+import logging
 
 from app.repositories.user_weight_repository import UserWeightRepository
 from app.repositories.action_log_repository import ActionLogRepository
-from app.services.user_service import process_user_action, update_user_weight, rollback_user_weight
+from app.services.user_service import process_user_action, update_user_weight
 from app.settings.local import settings
 import aio_pika
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -20,6 +21,9 @@ async def start_consumer():
     channel = await connection.channel()
     queue = await channel.declare_queue(QUEUE_NAME, durable=True)
 
+    mongo_client = AsyncIOMotorClient(settings.MONGO_URL)
+    user_repo = UserWeightRepository(mongo_client)
+    action_log_repo = ActionLogRepository(mongo_client)
     print(f"📡 Waiting for messages on queue: {QUEUE_NAME}")
 
     async with queue.iterator() as queue_iter:
@@ -29,16 +33,14 @@ async def start_consumer():
                 print(f"📥 Received: {body}")
 
                 data = json.loads(body)
-                mongo_client = AsyncIOMotorClient(settings.MONGO_URL)
-                user_repo = UserWeightRepository(mongo_client)
-                action_log_repo = ActionLogRepository(mongo_client)
 
                 try:
                     await update_user_weight(
                         data,
                         user_repo,
                     )
-                except Exception:
+                except Exception as e:
+                    logging.error(f"가중치 업데이트 실패 : {e}")
                     await action_log_repo.mark_status(
                         collection_names = ["action_log", "managed_action_log"],
                         doc_id = data["id"],
@@ -52,8 +54,8 @@ async def start_consumer():
                         user_repo,
                         action_log_repo
                     )
-                except Exception:
-                    await rollback_user_weight(data, user_repo)
+                except Exception as e:
+                    logging.error(f"사용자 벡터 갱신 실패 : {e}")
                     await action_log_repo.mark_status(
                         collection_names = ["action_log", "managed_action_log"],
                         doc_id = data["id"],
