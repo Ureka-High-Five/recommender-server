680ba313634619566f48e1693bb9280c32fea979
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-29 22:07:26 +0900

    hotfix : redis 실행 시점 디버그 코드 추가

--- app/services/redis.py
+++ app/services/redis.py
@@ -3,15 +3,28 @@ from app.settings import settings
 
 redis = None
 
+
+def get_redis() -> Redis:
+    if redis is None:
+        raise RuntimeError("Redis가 초기화되지 않았습니다.")
+    return redis
+
+
 async def init_redis():
     global redis
-    redis = Redis(host=settings.DEV_REDIS_HOST, port=settings.DEV_REDIS_PORT, decode_responses=True)
+    redis = Redis(
+        host=settings.DEV_REDIS_HOST,
+        port=settings.DEV_REDIS_PORT,
+        decode_responses=True,
+    )
     print(f"✅ Redis 연결 성공: {redis}")
+
 
 async def close_redis():
     if redis:
         await redis.close()
         print("🧹 Redis 연결 종료")
 
+
 async def save_user_vector(user_id: int, value: str):
-    await redis.set(user_id, value)
+    await get_redis().set(str(user_id), value)
